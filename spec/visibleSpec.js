23c1412e4f007b8e278e2750a9e47c50724b9f37
Andrey A.I. Sitnik <andrey@sitnik.ru>
2011-06-16 23:36:18 +0400

    Add method to return value of property with vendor prefix in name

--- spec/visibleSpec.js
+++ spec/visibleSpec.js
@@ -8,39 +8,50 @@ describe('Visible', function() {
 
     it('should detect, that browser use API without prefix', function () {
         document.visibilityState = 'visible'
-        expect(Visible._prefix()).toEqual('')
+        expect( Visible._prefix() ).toEqual('')
     })
 
     it('should detect vendor prefix', function() {
         document.mozVisibilityState = 'visible'
-        expect(Visible._prefix()).toEqual('moz')
+        expect( Visible._prefix() ).toEqual('moz')
     })
 
     it('should cache vendor prefix', function() {
         document.visibilityState = 'visible'
-        expect(Visible._prefix()).toEqual('')
+        expect( Visible._prefix() ).toEqual('')
 
         delete document.visibilityState
         document.webkitVisibilityState = 'visible'
-        expect(Visible._prefix()).toEqual('')
+        expect( Visible._prefix() ).toEqual('')
 
         Visible._chechedPrefix = null
-        expect(Visible._prefix()).toEqual('webkit')
+        expect( Visible._prefix() ).toEqual('webkit')
     })
 
     it('should detect Page Visibility support', function() {
-        expect(Visible.support()).toBeFalsy()
+        expect( Visible.support() ).toBeFalsy()
 
         document.webkitVisibilityState = 'visible'
         Visible._chechedPrefix = null
-        expect(Visible.support()).toBeTruthy()
+        expect( Visible.support() ).toBeTruthy()
     })
 
     it('should use properties with vendor prefix', function() {
         Visible._chechedPrefix = ''
-        expect(Visible._name('hidden')).toEqual('hidden')
+        expect( Visible._name('hidden') ).toEqual('hidden')
 
         Visible._chechedPrefix = 'webkit'
-        expect(Visible._name('hidden')).toEqual('webkitHidden')
+        expect( Visible._name('hidden') ).toEqual('webkitHidden')
+    })
+
+    it('should return value from property with vendor prefix', function() {
+        document.hidden = 2
+        document.webkitHidden = 1
+
+        Visible._chechedPrefix = 'webkit'
+        expect( Visible._prop('hidden') ).toEqual(1)
+
+        Visible._chechedPrefix = ''
+        expect( Visible._prop('hidden') ).toEqual(2)
     })
 })
