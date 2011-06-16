8a79d546a2be2c2d810b85d90268219d31c3c632
Andrey A.I. Sitnik <andrey@sitnik.ru>
2011-06-16 23:13:37 +0400

    Add code to detect vendor prefix

--- spec/visibleSpec.js
+++ spec/visibleSpec.js
@@ -1,1 +1,29 @@
-describe('Visible', function() { })
+describe('Visible', function() {
+    var document
+
+    beforeEach(function() {
+        Visible._doc = document = { }
+    })
+
+    it('should detect, that browser use API without prefix', function () {
+        document.visibilityState = 'visible'
+        expect(Visible._prefix()).toEqual('')
+    })
+
+    it('should detect vendor prefix', function() {
+        document.mozVisibilityState = 'visible'
+        expect(Visible._prefix()).toEqual('moz')
+    })
+
+    it('should cache vendor prefix', function() {
+        document.mozVisibilityState = 'visible'
+        expect(Visible._prefix()).toEqual('moz')
+
+        delete document.mozVisibilityState
+        document.webkitVisibilityState = 'visible'
+        expect(Visible._prefix()).toEqual('moz')
+
+        Visible._chechedPrefix = null
+        expect(Visible._prefix()).toEqual('webkit')
+    })
+})
