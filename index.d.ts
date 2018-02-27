1b96de349499582fa3433ecca9d46cc204f7b6ee
David Poetzsch-Heffter <davidpoetzsch@web.de>
2018-02-27 21:26:55 +0100

    fixed typings as described in issue #35 (#36)

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,13 @@
 declare module 'visibilityjs' {
-    const _default: Visibility;
-    export default _default;
-
-    class Visibility {
-        every(interval: number, callback: Function);
-
-        onVisible(callback: Function);
-
-        afterPrerendering(callback: Function);
-
-        isSupported(): boolean;
-
-        state(): string;
-
-        hidden(): boolean;
-
-        unbind(callback: Function);
-
-        change(listener: VisiblityChangeListener);
-    }
-
+    export function every(interval: number, callback: Function);
+    export function onVisible(callback: Function);
+    export function afterPrerendering(callback: Function);
+    export function isSupported(): boolean;
+    export function state(): string;
+    export function hidden(): boolean;
+    export function unbind(callback: Function);
+    export function change(listener: VisiblityChangeListener);
+    export function stop(listener: VisiblityChangeListener);
+  
     type VisiblityChangeListener = (event, state: string) => void;
 }
