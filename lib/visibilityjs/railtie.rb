175005edd290205192bdde6337f581e3f4da3ad7
Andrey Sitnik <andrey@sitnik.ru>
2018-05-22 02:53:02 +0300

    Remove another Ruby file

--- lib/visibilityjs/railtie.rb
+++
@@ -1,7 +0,0 @@
-# Add visibility.js path to the Rails assets paths.
-
-module VisibilityJs
-  class Railtie < Rails::Railtie
-    config.assets.configure { |env| VisibilityJs.install(env) }
-  end
-end
