38bfe0d4ca25f80819ee2960079008eac4f8f396
Dj <190334+dominiq-eu@users.noreply.github.com>
2024-01-11 12:01:43 +0100

    fix: typings

--- index.d.ts
+++ index.d.ts
@@ -4,11 +4,13 @@ declare module 'visibilityjs' {
     export function onVisible(callback: Function): number|boolean;
     export function afterPrerendering(callback: Function): number|boolean;
     export function isSupported(): boolean;
-    export function state(): string;
+    export function state(): VisibilityState;
     export function hidden(): boolean;
     export function unbind(id: number): void;
-    export function change(listener: VisiblityChangeListener): number|boolean;
+    export function change(listener: VisiblityChangeListener): number;
     export function stop(id: number): boolean;
 
-    type VisiblityChangeListener = (event: Event, state: string) => void;
+    // @See https://developer.mozilla.org/en-US/docs/Web/API/Document/visibilityState
+    type VisibilityState = 'visible' | 'hidden'
+    type VisiblityChangeListener = (event: Event, state: VisibilityState) => void;
 }
